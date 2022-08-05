1712516d3cf5a514c017afaa731756ce7257680f
kahall27 <kanehall27@gmail.com>
2022-08-05 13:08:28 -0400

    Allow user to select characters with mouse on game keyboard

--- german_wordle.py
+++ german_wordle.py
@@ -1,11 +1,19 @@
-from re import L
-from turtle import write_docstringdict
 import pygame
 from pygame.locals import *
 
 WINDOW_HEIGHT, WINDOW_WIDTH = 800, 500
 BACKGROUND = (144, 238, 144)
 GRAY = (100, 100, 100)
+
+row_one = ["Q", "W", "E", "R", "T", "Z", "U", "I", "O", "P", "Ü"]
+row_two = ["A", "S", "D", "F", "G", "H", "J", "K", "L", "Ö", "Ä"]
+row_three = ["Y", "X", "C", "V", "B", "N", "M"]
+rows = [row_one, row_two, row_three]
+
+# initially all letters are gray
+letter_colors = {letter: GRAY for letter in row_one + row_two + row_three}
+letter_locations = {letter: () for letter in row_one + row_two + row_three}
+letter_pressed = {letter: False for letter in row_one + row_two + row_three}
 
 def draw_boxes(screen):
     for i in range(6):
@@ -15,10 +23,6 @@ def draw_boxes(screen):
             pygame.draw.rect(screen, GRAY, pygame.Rect(from_left, from_top, 60, 60), border_radius=3)
 
 def draw_keyboard(screen):
-    row_one = ["Q", "W", "E", "R", "T", "Z", "U", "I", "O", "P", "Ü"]
-    row_two = ["A", "S", "D", "F", "G", "H", "J", "K", "L", "Ö", "Ä"]
-    row_three = ["Y", "X", "C", "V", "B", "N", "M"]
-    rows = [row_one, row_two, row_three]
 
     font = pygame.font.Font('freesansbold.ttf', 26)
     row_idx = 0
@@ -31,7 +35,8 @@ def draw_keyboard(screen):
             if row == row_three:
                 from_left += (WINDOW_WIDTH - 324) / 2    # center bottom row of keys
 
-            pygame.draw.rect(screen, GRAY, pygame.Rect(from_left, from_top, 30, 40), border_radius=3)
+            pygame.draw.rect(screen, letter_colors[row[i]], pygame.Rect(from_left, from_top, 30, 40), border_radius=3)
+            letter_locations[row[i]] = (from_left, from_top)
 
             text = font.render(row[i], True, (0, 0, 0))
             textRect = text.get_rect()
@@ -53,6 +58,7 @@ def main():
     # Fill background
     screen.fill(BACKGROUND)
 
+    current_word = "" 
 
     # Event loop
     while True:
@@ -62,6 +68,23 @@ def main():
             if event.type == QUIT:
                 return
 
+            if event.type == pygame.MOUSEBUTTONUP:
+                pos = pygame.mouse.get_pos()
+                if pos[1] >= 600 and pos[1] <= 640:
+                    for letter in row_one:
+                        if (pygame.Rect(letter_locations[letter][0], letter_locations[letter][1], 30, 40).collidepoint(pos[0], pos[1])) and (not letter_pressed[letter]):
+                            current_word += letter
+                            letter_pressed[letter] = not letter_pressed[letter] 
+                elif pos[1] >= 660 and pos[1] <= 700:
+                    for letter in row_two:
+                        if (pygame.Rect(letter_locations[letter][0], letter_locations[letter][1], 30, 40).collidepoint(pos[0], pos[1])) and (not letter_pressed[letter]):
+                            current_word += letter
+                            letter_pressed[letter] = not letter_pressed[letter]
+                elif pos[1] >= 720 and pos[1] <= 760:
+                    for letter in row_three:
+                        if (pygame.Rect(letter_locations[letter][0], letter_locations[letter][1], 30, 40).collidepoint(pos[0], pos[1])) and (not letter_pressed[letter]):
+                            current_word += letter
+                            letter_pressed[letter] = not letter_pressed[letter]
             # if event.type == pygame.KEYDOWN:
 
         screen.fill(BACKGROUND)
