2b3f6417790dfce44cfa1fbcaa0d629b2bde161f
kahall27 <kanehall27@gmail.com>
2022-08-21 13:16:00 -0400

    Update parse function to actually remove duplicates from five letter words txt file

--- parse_words.py
+++ parse_words.py
@@ -1,11 +1,12 @@
-five_letter_set = set()
 write_file = open("five_letter_words.txt", "w")
+
 with open("words.txt", "r") as word_file:
     prev_line = ""
     lines = word_file.readlines()
     for line in lines:
         if len(line.strip()) == 5 and (line.strip().lower() != prev_line):
-            write_file.write(line.strip().lower() + "\n")
+            prev_line = line.strip().lower()
+            write_file.write(prev_line + "\n")
 
 write_file.close()
             
