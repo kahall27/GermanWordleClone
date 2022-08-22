0f1a2b71d86c9bf86a0d04a8071eae8fae021e2c
kahall27 <kanehall27@gmail.com>
2022-08-22 11:39:26 -0400

    Fix binary search function to account for instances of umlauts in German words

--- binary_search.py
+++ binary_search.py
@@ -1,16 +1,30 @@
-# Iterative binary search
+# account for characters with umlauts (ä, ö, ü)
+def remove_umlauts(word):
+    if "ä" in word:
+        word = word.replace("ä", "a")
+    if "ö" in word:
+        word = word.replace("ö", "o")
+    if "ü" in word:
+        word = word.replace("ü", "u")
+    return word
 
+# Iterative binary search
 def binary_search(my_list, target):
+    target = remove_umlauts(target)
     lower_bound = 0
     upper_bound = len(my_list) - 1
     mid_point = 0
 
     while lower_bound <= upper_bound:
-        mid_point = (lower_bound + upper_bound) // 2
+        mid_point = lower_bound + ((upper_bound - lower_bound) // 2)
+        word_to_check = my_list[mid_point]
 
-        if my_list[mid_point] > target:
+        # account for characters with umlauts
+        word_to_check = remove_umlauts(word_to_check)
+
+        if word_to_check > target:
             upper_bound = mid_point - 1
-        elif my_list[mid_point] < target:
+        elif word_to_check < target:
             lower_bound = mid_point + 1
         else:
             return True
@@ -19,6 +33,6 @@ def binary_search(my_list, target):
 
 
 if __name__ == "__main__":
-    test_list = ["aa", "bb", "cc", "dd", "ee"]
-    print(binary_search(test_list, "aa")) # should print True
+    test_list = ["aa", "ää", "bb", "cc", "dd", "ee"]
+    print(binary_search(test_list, "ää")) # should print True
     print(binary_search(test_list, "ff")) # should print False
